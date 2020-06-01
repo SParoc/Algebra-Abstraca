15e07322a7ec52611f8ef0c167b483b8f3b1f10b
SParoc <51845735+SParoc@users.noreply.github.com>
2020-06-01 14:52:56 -0500

    Update AlgsCifrado.h

--- Cifrados_de_matrices__sin_matrices__/AlgsCifrado.h
+++ Cifrados_de_matrices__sin_matrices__/AlgsCifrado.h
@@ -7,20 +7,16 @@ class AlgsCifrado
 {
 public:
 	AlgsCifrado();
-
 	string monomioBinomio(string plainText, int num1, int num2, string mnemo);
 	string railFence(string plainText);
 	string descRailFence(string cipherText);
 	string polyBios(string plainText);
+	string descPolyBios(string cipherText);
 	string pb_abc = "abcdefghiklmnopqrstuvwxyz", pb_let = "ABCDE";
 	string mb_abc = "abcdefghijklmnopqrstuvwxyz", mb_num = "0123456789";
 
-
 private:
 
-	int pb_pos;
-	int mb_pos;
 	string mb_abc2;
-
 	int rf_key;
 };
