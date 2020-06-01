ec636ada4d9393007d3f9cc86218b1de50689600
SParoc <51845735+SParoc@users.noreply.github.com>
2020-06-01 14:52:17 -0500

    Update main.cpp

--- Cifrados_de_matrices__sin_matrices__/main.cpp
+++ Cifrados_de_matrices__sin_matrices__/main.cpp
@@ -6,26 +6,30 @@ int main()
     cout << "3 algoritmos de cifrado\n\n";
 
     AlgsCifrado cifrado;
-    string rf_str, pb_str, mb_str;
+    string str;
    
     int op;
 
-    cout << "1. Monomio-Binomio\n2. Rail Fence\n3. Polybios\n\n";
+    cout << "1. Monomio-Binomio\n2. Rail Fence\n3. Polybios\n4. Descifrado PolyBios\n\n";
     cin >> op;
 
     switch (op)
     {
     case 1:
-        getline(cin.ignore(), mb_str);
-        cout << "Cifrado: " << cifrado.monomioBinomio(mb_str, 2, 7, "denarios") << "\n\n";
+        getline(cin.ignore(), str);
+        cout << "Cifrado: " << cifrado.monomioBinomio(str, 2, 7, "denarios") << "\n\n";
         break;
     case 2:
-        getline(cin.ignore(), rf_str);
-        cout << "Cifrado: " << cifrado.railFence(rf_str) << "\n\n";
+        getline(cin.ignore(), str);
+        cout << "Cifrado: " << cifrado.railFence(str) << "\n\n";
         break;
     case 3:
-        getline(cin.ignore(), pb_str);
-        cout << "Cifrado: " << cifrado.polyBios(pb_str) << "\n\n";
+        getline(cin.ignore(), str);
+        cout << "Cifrado: " << cifrado.polyBios(str) << "\n\n";
+        break;
+    case 4:
+        getline(cin.ignore(), str);
+        cout << "Descifrado: " << cifrado.descPolyBios(str) << "\n\n";
         break;
     default:
         cout << "Opcion no valida\n"; 
