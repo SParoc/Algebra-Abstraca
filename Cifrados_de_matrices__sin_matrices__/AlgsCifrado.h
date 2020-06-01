0679c6019c5f5218817d8f6502f8be40a592c525
SParoc <51845735+SParoc@users.noreply.github.com>
2020-06-01 12:21:14 -0500

    Update AlgsCifrado.h

--- Cifrados_de_matrices__sin_matrices__/AlgsCifrado.h
+++ Cifrados_de_matrices__sin_matrices__/AlgsCifrado.h
@@ -8,18 +8,19 @@ class AlgsCifrado
 public:
 	AlgsCifrado();
 
-	string monomioBinomio(string plainText, string cipherText, int num1, int num2, string mnemo);
-	string railFence(string plainText, string cipherText, int keylave);
-	string polyBios(string plainText, string cipherText);
+	string monomioBinomio(string plainText, int num1, int num2, string mnemo);
+	string railFence(string plainText);
+	string descRailFence(string cipherText);
+	string polyBios(string plainText);
 	string pb_abc = "abcdefghiklmnopqrstuvwxyz", pb_let = "ABCDE";
 	string mb_abc = "abcdefghijklmnopqrstuvwxyz", mb_num = "0123456789";
 
 
-	
 private:
-	int rf_space, rf_space2, rf_begin, rf_pos;
-	bool rf_swap;
+
 	int pb_pos;
 	int mb_pos;
 	string mb_abc2;
+
+	int rf_key;
 };
